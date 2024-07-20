6e4223b456fe7de28bffd774b03ddcef441f0e7e
Dev Fred <manfredtochukwu362@gmail.com>
2024-07-20 15:23:51 +0100

    Added WebSocket connection and PumpCard component to TokenExplorer, updated CSS files

---
+++ ui/src/components/PumpCard.tsx
@@ -0,0 +1,14 @@
+export default function PumpCard() {
+
+  
+
+  return (
+    <div className="pump-card">
+      <a className="w-full h-full absolute inset-0 z-[1]" href="/terminal?chainId=1399811149&amp;address=2FxuYGVZRoUXg51zffnYxymqerFAuSEQabAsLzTFpump">
+      </a>
+
+    </div>
+  )
+}
+
+
