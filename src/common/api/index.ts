17f8f2167131f29e19316f7cd653f1bf81bb1f63
Dev Fred <manfredtochukwu362@gmail.com>
2024-07-22 22:44:14 +0100

    Remove pump API routes and update getPumpList orderby parameter

--- src/common/api/index.ts
+++ src/common/api/index.ts
@@ -4,17 +4,14 @@ import { IPumpCoin, PumpDetail } from '../../types';
 const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
 export async function getPumpList(): Promise<IPumpCoin[]> {
-
   const params = new URLSearchParams({
     limit: '150',
-    orderby: 'progress',
+    orderby: 'marketcap',
     direction: 'desc',
     pump: 'true',
   });
-
   const res = await fetch(`${corsProxyUrl}https://gmgn.ai/defi/quotation/v1/rank/sol/pump?${params.toString()}`, { cache: "no-store" });
   const data = await res.json()
-  console.log({data})
   if (data.code === 0) {
     return data.data.rank
   }
