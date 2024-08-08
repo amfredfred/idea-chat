98a9fe86753fc6bb3cc866c873cb83dcf2e01941
Editor Fred <amdevfred@gmail.com>
2024-08-09 00:53:55 +0100

    Refactored Chaos component: added useCallback and debounce for handling resize events, updated grid dimensions and animation styles, and made various code organization changes.

--- src/components/message-animations/Chaos.tsx
+++ src/components/message-animations/Chaos.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { motion } from 'framer-motion';
-import { useMediaQuery } from '@mui/material';
-import { addNewMessage, Message } from '../../libs/redux/slices/chat-slice';
 import { useAppDispatch, useAppSelector } from '../../libs/redux/hooks';
+import { addNewMessage, Message } from '../../libs/redux/slices/chat-slice';
 import { generateRandomHex } from '../../utils';
+import debounce from 'lodash/debounce';
 
 interface MessageItem extends Message {
   _id: string;
@@ -13,16 +13,31 @@ interface MessageItem extends Message {
   timestamp: number;
   marginClass?: string;
   textClampClass?: string;
-  isEmpty: boolean
+  isEmpty: boolean;
 }
 
+const getRandomRotation = (min: number = 0, max: number = 20) => {
+  const rotation = Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.random() > 0.5 ? rotation : -rotation;
+};
+
 const Chaos: React.FC = () => {
-  const isLargeScreen = useMediaQuery('(min-width: 1200px)');
-  const isMediumScreen = useMediaQuery('(min-width: 600px) and (max-width: 1199px)');
-  const numColumns = isLargeScreen ? 5 : (isMediumScreen ? 3 : 2);
-  const numRowsPerColumn = isLargeScreen ? 4 : (isMediumScreen ? 3 : 4);
-  const newMessage = useAppSelector(state => state.chat.newMessage);
   const dispatch = useAppDispatch();
+  const newMessage = useAppSelector((state) => state.chat.newMessage);
+
+  const getGridDimensions = () => {
+    if (window.innerWidth >= 1200) {
+      return { numColumns: 5, numRowsPerColumn: 4, totalSlots: 20 };
+    } else if (window.innerWidth >= 600) {
+      return { numColumns: 3, numRowsPerColumn: 3, totalSlots: 9 };
+    } else {
+      return { numColumns: 2, numRowsPerColumn: 4, totalSlots: 8 };
+    }
+  };
+
+
+
+  const [gridConfig, setGridConfig] = useState(getGridDimensions);
 
   const generateRandomStyles = () => {
     return {
@@ -36,27 +51,57 @@ const Chaos: React.FC = () => {
     const newMsg: MessageItem = {
       _id: generateRandomHex(),
       message: `In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available`,
-      username: `User_${Math.floor(Math.random() * 10)}`,
+      username: `User_${generateRandomHex(10)}`,
       profilePic: `https://randomuser.me/api/portraits/men/${Math.floor(Math.random() * 50)}.jpg`,
       timestamp: Date.now(),
       isEmpty,
       textClampClass
-    }
-    return newMsg
-  }
+    };
+    return newMsg;
+  };
 
   const [gridData, setGridData] = useState<MessageItem[]>(() => {
     const data = [] as MessageItem[];
-    for (let colIndex = 0; colIndex < numColumns; colIndex++) {
-      for (let rowIndex = 0; rowIndex < numRowsPerColumn; rowIndex++) {
-        const { marginClass, textClampClass } = generateRandomStyles();
-        data.push({ ...makeMessage(true), message: '', marginClass, textClampClass });
-      }
+    const { totalSlots } = getGridDimensions();
+    for (let i = 0; i < totalSlots; i++) {
+      const { marginClass, textClampClass } = generateRandomStyles();
+      data.push({ ...makeMessage(true), message: '', marginClass, textClampClass });
     }
-    return data
+    return data;
   });
 
-  const getEmptyRows = () => gridData.filter(item => item.isEmpty);
+  const adjustGridData = (newTotalSlots: number) => {
+    setGridData((prevData) => {
+      const currentTotalSlots = prevData.length;
+      if (newTotalSlots > currentTotalSlots) {
+        const slotsToAdd = newTotalSlots - currentTotalSlots;
+        const newItems = Array.from({ length: slotsToAdd }, () => makeMessage(true));
+        return [...prevData, ...newItems];
+      } else if (newTotalSlots < currentTotalSlots) {
+        const slotsToRemove = currentTotalSlots - newTotalSlots;
+        return prevData.slice(0, currentTotalSlots - slotsToRemove);
+      }
+      return prevData;
+    });
+  };
+
+  const updateGridWithNewMessage = () => {
+    setGridData((prevData) => {
+      const emptyRows = prevData.filter((item) => item.isEmpty);
+      if (emptyRows.length > 0) {
+        const randomEmptyRow = emptyRows[Math.floor(Math.random() * emptyRows.length)];
+        return prevData.map((item) =>
+          item._id === randomEmptyRow._id ? { ...item, ...newMessage, isEmpty: false } : item
+        );
+      } else {
+        const randomNonRecentMessage = getRandomNonRecentMessage(prevData);
+        return prevData.map((item) =>
+          item._id === randomNonRecentMessage._id ? { ...item, ...newMessage, isEmpty: false } : item
+        );
+      }
+    });
+  };
+
   const getRandomNonRecentMessage = (messages: MessageItem[]) => {
     const sortedMessages = [...messages].sort((a, b) => b.timestamp - a.timestamp);
     const mostRecentMessage = sortedMessages[0];
@@ -65,56 +110,55 @@ const Chaos: React.FC = () => {
     return nonRecentMessages[Math.floor(Math.random() * nonRecentMessages.length)];
   };
 
-  const updateGridWithNewMessage = () => {
-    setGridData(prevData => {
-      const emptyRows = getEmptyRows();
-      if (emptyRows.length > 0) {
-        const randomEmptyRow = emptyRows[Math.floor(Math.random() * emptyRows.length)];
-        return prevData.map(item => item._id == randomEmptyRow._id ? { ...item, ...newMessage } : item);
-      } else {
-        const randomNonRecentMessage = getRandomNonRecentMessage(prevData);
-        return prevData.map(item => item._id == randomNonRecentMessage._id ? { ...item, ...newMessage } : item);
-      }
-    });
-  };
+  const handleResize = useCallback(() => {
+    const newConfig = getGridDimensions();
+    adjustGridData(newConfig.totalSlots);
+    setGridConfig(newConfig);
+  }, []);
+
+  const debouncedHandleResize = useCallback(debounce(handleResize, 300), [handleResize]);
 
   useEffect(() => {
-    if (newMessage)
-      updateGridWithNewMessage();
+    if (newMessage) updateGridWithNewMessage();
   }, [newMessage]);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       dispatch(addNewMessage(makeMessage()));
-    }, 1000);
-    return () => clearInterval(intervalId);
-  }, []);
+    }, 200);
+
+    window.addEventListener('resize', debouncedHandleResize);
+
+    return () => {
+      clearInterval(intervalId);
+      window.removeEventListener('resize', debouncedHandleResize);
+    };
+  }, [debouncedHandleResize, dispatch]);
 
   return (
-    <div className="flex flex-col h-full w-full">
-      <h1>messages count: {gridData.length} with message {gridData.filter(msg => msg.message !== '').length}</h1>
-      <div className={`grid grid-cols-${numColumns} gap-4 flex-1`}>
+    <div className="flex flex-col h-full w-full overflow-hidden">
+      <div className={`grid grid-cols-${gridConfig.numColumns} gap-4 flex-1 overflow-hidden`}>
         {gridData.map(({ _id, message: msg, username, profilePic, marginClass, textClampClass }) => (
           <motion.div
             key={_id}
-            initial={{ opacity: 0, scale: 0.5, rotate: -20 }}
-            animate={{ opacity: 1, scale: 1, rotate: 0 }}
-            exit={{ opacity: 0, scale: 0.5, rotate: 20 }}
+            initial={{ opacity: 0, scale: 0.5, rotate: getRandomRotation(), y: getRandomRotation(), x: getRandomRotation() }}
+            animate={{ opacity: 1, scale: 1, rotate: getRandomRotation(), y: getRandomRotation(), x: getRandomRotation() }}
+            exit={{ opacity: 0, scale: 0.5, rotate: getRandomRotation(), y: getRandomRotation(), x: getRandomRotation() }}
             transition={{ type: 'spring', stiffness: 100, damping: 20 }}
             className={`flex items-center`}
           >
-            {msg ? (<div className={`flex flex-col  ${marginClass}`}>
-              <div className="flex items-center gap-2 overflow-hidden max-sm:items-start">
-                <div className="flex items-center gap-2">
+            {msg ? (
+              <div className={`flex flex-col ${marginClass}`}>
+                <div className="flex items-center gap-2 overflow-hidden max-sm:items-start">
                   <img src={profilePic} alt={username} className="w-6 h-6 rounded-full" />
-                  <p className="font-bold text-xs max-sm:hidden">{username}</p>
-                </div>
-                <div className="flex-1 flex flex-col justify-start">
-                  <p className="font-bold text-xs max-sm:block">{username}</p>
-                  <p className={`${textClampClass} text-xs`}>{msg}</p>
+                  <p className="font-bold text-xs max-sm:hidden text-ellipsis overflow-hidden max-w-10 ">{username}</p>
+                  <div className="flex-1 flex flex-col justify-start ">
+                    <p className="font-bold text-xs max-sm:block">{username}</p>
+                    <p className={`${textClampClass} text-xs`}>{msg}</p>
+                  </div>
                 </div>
               </div>
-            </div>) : (
+            ) : (
               <p className={`${textClampClass}`}>&nbsp;</p>
             )}
           </motion.div>
